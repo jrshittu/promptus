bfe5287041b0b267fc6fb1550655d0f200ee0fd4
Abideen <misterjrshittu@gmail.com>
2023-09-04 21:36:08 +0100

    Update Page

--- app/Page.jsx
+++ app/Page.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className="w-full flex-center flex-col">
+      <h1>
+        Discover & Share
+        <br className='max-md:hidden'/>
+        <span className='orange_gradient'>AI-Powered Prompts</span>
+      </h1>
+      
+    </section>
   )
 }
 
