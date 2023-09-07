4d8b40ccd843e5a075ef6fff3a2bf4c628fbe728
Abideen <misterjrshittu@gmail.com>
2023-09-07 10:47:05 +0100

    Update db

--- utils/database.js
+++ utils/database.js
@@ -4,5 +4,18 @@ let isConnected = false;
 
 export const connectToDB = async () => {
     mongoose.set('strictQuery', true);
-    
+
+    if(isConnected){
+        console.log("MongoDB is already connected");
+        return;
+    }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "promptus";
+            useNewUrlParser: true,
+            
+        })
+    } catch (error) {
+        
+    }
 }
