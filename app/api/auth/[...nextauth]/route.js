5b0465a9bc643ebea95a6ccf0e438590de31ba08
Abideen <misterjrshittu@gmail.com>
2023-09-07 12:51:01 +0100

    Add Google Auth and Finish User Profile

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 import { connectToDB } from '@utils/database';
+import User from '@models/user';
 
 const handler = NextAuth({
     providers: [
@@ -10,11 +11,32 @@ const handler = NextAuth({
         })
     ],
     async session({ session }){
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        });
+
+        session.user.id = sessionUser._id.toString();
+
+        return session;
 
     },
     async signIn({ profile }){
         try {
             await connectToDB();
+
+            // check if user exists
+            const userExists = await User.findOne({
+                email: profile.mail
+            });
+
+            // if does'nt exists, create a new user
+            if(!userExists){
+                await User.create({
+                     email: profile.email,
+                     username: profile.name.replace(" ", "").toLowerCase(),
+                     image: profile.picture
+                });
+            }
 
             return true;
         } catch (error) {
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,20 +8,20 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
 
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
 
   const [ providers, setProviders ] = useState(null);
   const [ toggleDropdown, setToggleDropdown ] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
 
       const response = await getProviders();
 
       setProviders(response);
 
     }
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -37,9 +37,11 @@ const Nav = () => {
         <p className='logo_text'>Promptus</p>
       </Link>
 
+
+
       {/* Desktop Nav */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href="/create-prompt" className='black_btn'>
               Create Post
@@ -49,7 +51,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image 
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded_full'
@@ -79,10 +81,10 @@ const Nav = () => {
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
       {
-        isUserLoggedIn ? (
+        session?.user ? (
           <div className='flex'>
             <Image 
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded_full'
